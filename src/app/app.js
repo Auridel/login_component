34f3a70612bec8987ec434a666c3d13dbeb8e2bc
Auridel <31318127+Auridel@users.noreply.github.com>
2020-10-18 16:36:46 +0300

    init

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Tabs, Tab} from "react-materialize";
+
+const App = () => {
+    return (
+        <div className="auth-component">
+
+        </div>
+    )
+};
+
+export default App;
